dd0ed356bbe138927e879bb460e9fdae1622c88e
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-06 18:49:12 +0530

    Add: Validate Last Name

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -11,6 +11,10 @@ namespace UserRegistration
             string firstName = Console.ReadLine();
             bool fNameResult = validateUser.ValidateFirstName(firstName);
             validateUser.PrintResult(fNameResult);
+            Console.WriteLine("Enter your last name: ");
+            string lastName = Console.ReadLine();
+            bool lNameResult = validateUser.ValidateLastName(lastName);
+            validateUser.PrintResult(lNameResult);
         }
     }
 }
--- UserRegistration/UserRegistration/RegexValidate.cs
+++ UserRegistration/UserRegistration/RegexValidate.cs
@@ -8,9 +8,14 @@ namespace UserRegistration
     class RegexValidate
     {
         public static string Regex_FirstName = "^[A-Z]{1}[a-z]{2,}$";
+        public static string Regex_LastName = "^[A-Z]{1}[a-z]{2,}$";
         public bool ValidateFirstName(string first)
         {
             return Regex.IsMatch(first, Regex_FirstName);
+        }
+        public bool ValidateLastName(string last)
+        {
+            return Regex.IsMatch(last, Regex_LastName);
         }
         public void PrintResult(bool result)
         {
