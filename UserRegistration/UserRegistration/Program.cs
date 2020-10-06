f9c8d14da20c1d9c9a2b02bf2e4e8ff77415a306
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-06 18:39:39 +0530

    Add: Validate First Name

---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexValidate validateUser = new RegexValidate();
+            Console.WriteLine("Enter your first name: ");
+            string firstName = Console.ReadLine();
+            bool fNameResult = validateUser.ValidateFirstName(firstName);
+            validateUser.PrintResult(fNameResult);
+        }
+    }
+}
---
+++ UserRegistration/UserRegistration/RegexValidate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class RegexValidate
+    {
+        public static string Regex_FirstName = "^[A-Z]{1}[a-z]{2,}$";
+        public bool ValidateFirstName(string first)
+        {
+            return Regex.IsMatch(first, Regex_FirstName);
+        }
+        public void PrintResult(bool result)
+        {
+            if (result)
+            {
+                Console.WriteLine("Valid.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid.");
+            }
+        }
+    }
+}
