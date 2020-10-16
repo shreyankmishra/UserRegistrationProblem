8c41a2c79c74bfd3761fe0f91e21ceb03909a9f5
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-16 19:49:19 +0530

    Refactor: Throw Custom Exceptions for Invalid User Details

--- UserRegistration/UserRegistrationTest/UnitTest1.cs
+++ UserRegistration/UserRegistrationTest/UnitTest1.cs
@@ -6,60 +6,90 @@ namespace UserRegistrationTest
     [TestClass]
     public class UnitTest1
     {
-            [TestMethod]
-            public void FistNameTest()
-            {
+        [TestMethod]
+        public void FirstNameTest()
+        {
+            try {
                 //Arrange
                 RegexValidate user = new RegexValidate();
                 string firstName = "Shreyank";
                 //Act
                 bool result = user.ValidateFirstName(firstName);
-                //Assert
-                Assert.IsTrue(result);
             }
-            [TestMethod]
-            public void LastNameTest()
+            catch (InvalidUserDetailsCustomException e)
             {
+                //Assert
+                Assert.AreEqual("Invalid password.", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void LastNameTest()
+        {
+            try {
                 //Arrange
                 RegexValidate user = new RegexValidate();
                 string lastName = "Mishra";
                 //Act
                 bool result = user.ValidateLastName(lastName);
-                //Assert
-                Assert.IsTrue(result);
             }
-            [TestMethod]
-            public void EmailIdTest()
+            catch (InvalidUserDetailsCustomException e)
             {
+                //Assert
+                Assert.AreEqual("Invalid password.", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void EmailIdTest()
+        {
+            try {
                 //Arrange
                 RegexValidate user = new RegexValidate();
                 string email = "mishra.shreyank@gmail.com";
                 //Act
                 bool result = user.ValidateEmail(email);
-                //Assert
-                Assert.IsTrue(result);
             }
-            [TestMethod]
-            public void MobileNumberTest()
+            catch (InvalidUserDetailsCustomException e)
             {
+                //Assert
+                Assert.AreEqual("Invalid password.", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void MobileNumberTest()
+        {
+            try {
                 //Arrange
                 RegexValidate user = new RegexValidate();
                 string mobileNumber = "91 7598624310";
                 //Act
                 bool result = user.ValidateMobileNo(mobileNumber);
-                //Assert
-                Assert.IsTrue(result);
             }
+            catch (InvalidUserDetailsCustomException e)
+            {
+                //Assert
+                Assert.AreEqual("Invalid password.", e.Message);
+            }
+        }
+     
             [TestMethod]
             public void PasswordTest()
+            {
+            try
             {
                 //Arrange
                 RegexValidate user = new RegexValidate();
                 string password = "SAmmy@StRiker1";
                 //Act
                 bool result = user.ValidatePassword(password);
+            }
+            catch (InvalidUserDetailsCustomException e)
+            {
                 //Assert
-                Assert.IsTrue(result);
+                Assert.AreEqual("Invalid password.", e.Message);
+            }
             }
         [TestMethod]
         [DataRow("abc@yahoo.com")]
