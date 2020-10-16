9aada14839064ce831d642352d07cebb8be8a635
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-16 19:23:07 +0530

    Add: Validate multiple entry Email Address Test

--- UserRegistration/UserRegistrationTest/UnitTest1.cs
+++ UserRegistration/UserRegistrationTest/UnitTest1.cs
@@ -61,5 +61,24 @@ namespace UserRegistrationTest
                 //Assert
                 Assert.IsTrue(result);
             }
+        [TestMethod]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void ValidateMultipleEmailsTest(string email)
+        {
+            //Arrange
+            RegexValidate user = new RegexValidate();
+            //Act
+            bool result = user.ValidateEmailSamples(email);
+            //Assert
+            Assert.IsTrue(result);
         }
+    }
 }
