b02cd1dbc15624f3420a2a0a26ef63c433fd8529
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-16 21:28:30 +0530

    Refactor: Throw Custom Exceptions for invalid user detail

---
+++ UserRegistration/UserRegistration/InvalidUserDetailsCustomException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserRegistration
+{
+       public class InvalidUserDetailsCustomException : Exception
+        {
+            public enum CustomExceptionType
+            {
+                INVALID_FIRST_NAME,
+                INVALID_LAST_NAME,
+                INVALID_EMAIL,
+                INVALID_MOBILE_NUMBER,
+                INVALID_PASSWORD
+            }
+            private CustomExceptionType type;
+            public InvalidUserDetailsCustomException(CustomExceptionType type, string message) : base(message)
+            {
+                this.type = type;
+            }
+        }
+    }
+
